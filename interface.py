5f1eaeb5bb152f0d8bc317b77179d1b74850fb46
Ted Kalaw <ted.kalaw@gmail.com>
2012-01-08 01:27:05 -0600

    Adds exit on token check and fixed some stupid stuff I was doing

--- interface.py
+++ interface.py
@@ -7,19 +7,25 @@ TOKEN_GENERATOR = "https://trello.com/1/connect?key={0}&name=trello%20commandlin
 parser = argparse.ArgumentParser(description='Commandline interface for Trello.',
                                   epilog="You can generate your user token by going to: {0}".format(TOKEN_GENERATOR))
 parser.add_argument('-token', action='store', help='Your unique user token from Trello.')
+parser.add_argument('--t', action='store_true', help='Display token')
 args = parser.parse_args()
 
 if args.token:
-  with open('./.token', mode='w', encoding='utf-8') as store:
+  with open('.token', mode='w', encoding='utf-8') as store:
     store.write(args.token)
     print("Successfuly stored token.")
 
 try:
-  _token_file = open('./.token', encoding='utf-8')
+  _token_file = open('.token', encoding='utf-8')
 except IOError:
   print("You need to add your user token.\nThis can be generated at:\n{0}".format(TOKEN_GENERATOR))
 
 token = _token_file.read()
 _token_file.close()
 
+if args.t:
+  print('Your token is:\n{0}'.format(token))
+  exit()
+
 ti = trello.TrelloInterface(TEDS_KEY, token)
+print(ti.request_url("members/me"))
--- trello.py
+++ trello.py
@@ -1,10 +1,27 @@
 import httplib2
+#Response headers are STRING
+#Content body is BYTES
 
 class TrelloInterface:
   def __init__(self, app_key, token):
     self.app_key = app_key
     self.token = token
+    self.h = httplib2.Http('.cache')
 
-  def request_url(action)
-    return 'api.trello.com/{0}?key={1}&token={2}'.format(action, self.app_key, self.token)
-  end
+  def request_url(self, action):
+    return 'https://api.trello.com/1/{0}?key={1}&token={2}'.format(action, self.app_key, self.token)
+
+class Board:
+  pass
+
+class List:
+  pass
+
+class Card:
+  pass
+
+class Checklist:
+  pass
+
+class Member:
+  pass
