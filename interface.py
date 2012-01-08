bca98a865d1b0f2a732932525ec65c2a34583cd8
Ted Kalaw <ted.kalaw@gmail.com>
2012-01-07 23:36:58 -0600

    Token information and some parsing added

--- interface.py
+++ interface.py
@@ -1,5 +1,15 @@
 import trello
+import argparse
 
-ti = trello.TrelloInterface("hi")
-print(ti.app_key)
-print(ti.token)
+TEDS_KEY = '32adf82c057290a622fd71ba0638b428'
+TOKEN_GENERATOR = "https://trello.com/1/connect?key=32adf82c057290a622fd71ba0638b428&name=trello%20commandline%20interface&response_type=token&context=read,write"
+#Setup various args
+parser = argparse.ArgumentParser(description='Commandline interface for Trello.',
+                                  epilog="You can generate your user token by going to: {0}".format(TOKEN_GENERATOR))
+parser.add_argument('-token', action='store', help='Your unique user token.')
+args = parser.parse_args()
+
+if args.token:
+  print(args.token)
+
+#let's start it up
--- trello.py
+++ trello.py
@@ -1,12 +1,13 @@
 import urllib
 
 class TrelloInterface:
-  def __init__(self, token):
+  def __init__(self, token, app_key):
     #will need to figure out what the proper thing to do with this is
-    self.app_key = "32adf82c057290a622fd71ba0638b428"
-    self.token = "2869245a05aaff93daf7a044e2ceb3b9"
-    
+    self.app_key = app_key
+    self.token = token
 
+#    self.token = "2869245a05aaff93daf7a044e2ceb3b9"
+    
 #note:
 #it should be 
-#api.trello.com/queryorwhateveritis  **?key=self.app_key&?token=self.token**
+#api.trello.com/queryorwhateveritis  **?key=self.app_key&token=self.token**
