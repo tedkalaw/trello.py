ba89bfea930a89143a39066751e2dabed1efbc21
Ted Kalaw <ted.kalaw@gmail.com>
2012-01-08 00:01:06 -0600

    Adds in token saving

--- interface.py
+++ interface.py
@@ -2,14 +2,14 @@ import trello
 import argparse
 
 TEDS_KEY = '32adf82c057290a622fd71ba0638b428'
-TOKEN_GENERATOR = "https://trello.com/1/connect?key=32adf82c057290a622fd71ba0638b428&name=trello%20commandline%20interface&response_type=token&context=read,write"
-#Setup various args
+TOKEN_GENERATOR = "https://trello.com/1/connect?key={0}&name=trello%20commandline%20interface&response_type=token&context=read,write".format(TEDS_KEY)
+
 parser = argparse.ArgumentParser(description='Commandline interface for Trello.',
                                   epilog="You can generate your user token by going to: {0}".format(TOKEN_GENERATOR))
-parser.add_argument('-token', action='store', help='Your unique user token.')
+parser.add_argument('-token', action='store', help='Your unique user token from Trello.')
 args = parser.parse_args()
 
 if args.token:
-  print(args.token)
+  with open('./.token', mode='w', encoding='utf-8') as store:
+    store.write(args.token)
 
-#let's start it up
